8a604caf1bb408713e56b8fb6a5a8726b7c0b40d
Reuben <2824016820@qq.com>
2021-07-09 15:58:29 +0800

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char a;
+    int index = 1;
+    while (true){
+        cout << "-------主存管理-------" << endl;
+        cout << "输入q退出，输入s存，输入d删" << endl;
+        cout << index++ << ": ";
+        cin >> a;
+        switch (a) {
+            case 'q':
+                goto L1;
+            case 's':
+                cout << "存" << endl;
+                break;
+            case 'd':
+                cout << "删" << endl;
+                break;;
+            default:
+                break;
+        }
+    }
+L1:
+    cout << "退出成功" << endl;
     return 0;
 }
